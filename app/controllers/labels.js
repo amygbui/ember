83e18eed25abe9cb733e146587586409c7dd662a
Amy Bui <amygbui@gmail.com>
2017-06-15 20:46:39 -0400

    Add delete functionality to Labels

--- app/controllers/labels.js
+++ app/controllers/labels.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // validators: [{ field: 'name', validator: 'required', message: "Name can't be blank"}],
-  // actions: {
-  //   addLabel(data) {
-  //     this.store.createRecord('label', {
-  //       name: this.get('model.name')
-  //     })
-  //     this.set('model.name', '')
-  //   }
-  // }
 });
--- app/routes/labels/show.js
+++ app/routes/labels/show.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('label', params.label_id);
+  },
+  actions: {
+    deleteLabel(model) {
+      model.destroyRecord()
+          .then(() =>(this.transitionTo('labels.index')))
+    }
   }
 });
