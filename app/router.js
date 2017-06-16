f363baee7396c9addea6f28bfedd8ed7823e4216
Amy Bui <amygbui@gmail.com>
2017-06-16 15:01:55 -0400

    Get all show pages working for all models

--- app/router.js
+++ app/router.js
@@ -12,6 +12,18 @@ Router.map(function() {
     this.route('new');
     this.route('edit', { path: 'edit/:label_id' });
   });
+
+  this.route('artists', function() {
+    this.route('show', { path: '/:artist_id' });
+  });
+
+  this.route('albums', function() {
+    this.route('show', { path: '/:album_id' });
+  });
+
+  this.route('songs', function() {
+    this.route('show', { path: '/:song_id' });
+  });
 });
 
 export default Router;
