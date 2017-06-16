4b6f7eef5a2c5f327ab0e7c7f457a2b2a78d3629
Amy Bui <amygbui@gmail.com>
2017-06-15 21:24:55 -0400

    Finish CRUD for Labels

--- app/routes/labels/edit.js
+++ app/routes/labels/edit.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('label', params.label_id);
+  },
+  actions: {
+    updateLabel(model) {
+      this.store.findRecord('label', model.id)
+        .then(label => {
+          label.set('name', this.get('controller.model.name'));
+          label.save();
+        })
+        .then(this.transitionTo('labels.show', model.id))
+    }
   }
 });
