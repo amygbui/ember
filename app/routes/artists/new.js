625a7635350c80a818a4d682270a0bf1fcbcc2cf
Amy Bui <amygbui@gmail.com>
2017-06-17 00:46:10 -0400

    Finalize Create for Artists

--- app/routes/artists/new.js
+++ app/routes/artists/new.js
@@ -2,16 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('artist');
+    return this.store.findAll('label');
   },
   actions: {
     addArtist() {
       const newArtist = this.store.createRecord('artist', {
         name: this.get('controller.model.name')
       })
-
       this.set('controller.model.name', '')
-      this.store.findRecord('label', 2)
+      this.store.findRecord('label', this.get('controller.model.label'))
                       .then(label => {
                         newArtist.save();
                         label.get('artists').pushObject(newArtist);
