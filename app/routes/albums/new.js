b669d07cff71d23c0fae332e1c8509ccdae21f91
Amy Bui <amygbui@gmail.com>
2017-06-17 22:40:43 -0400

    Fix minor spelling errors on forms

--- app/routes/albums/new.js
+++ app/routes/albums/new.js
@@ -9,14 +9,14 @@ export default Ember.Route.extend({
       const newAlbum = this.store.createRecord('album', {
         title: this.get('controller.model.name')
       })
-      this.set('controller.model.name', '')
+      this.set('controller.model.name', '');
       this.store.findRecord('artist', this.get('controller.model.artist_id'))
-                      .then(artist => {
-                        newAlbum.save();
-                        artist.get('albums').pushObject(newAlbum);
-                        artist.save();
-                      })
-      this.transitionTo('albums.show', newAlbum.id)
+        .then(artist => {
+          newAlbum.save();
+          artist.get('albums').pushObject(newAlbum);
+          artist.save();
+        })
+      this.transitionTo('albums.show', newAlbum.id);
     }
   }
 });
--- app/routes/artists/new.js
+++ app/routes/artists/new.js
@@ -9,14 +9,14 @@ export default Ember.Route.extend({
       const newArtist = this.store.createRecord('artist', {
         name: this.get('controller.model.name')
       })
-      this.set('controller.model.name', '')
+      this.set('controller.model.name', '');
       this.store.findRecord('label', this.get('controller.model.label_id'))
-                      .then(label => {
-                        newArtist.save();
-                        label.get('artists').pushObject(newArtist);
-                        label.save();
-                      })
-      this.transitionTo('artists.show', newArtist.id)
+        .then(label => {
+          newArtist.save();
+          label.get('artists').pushObject(newArtist);
+          label.save();
+        })
+      this.transitionTo('artists.show', newArtist.id);
     }
   }
 });
