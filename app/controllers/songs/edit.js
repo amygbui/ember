c3b5dae746f94188a3304c9f5859a22ada7ba222
Amy Bui <amygbui@gmail.com>
2017-06-18 01:35:01 -0400

    Remove console.log

--- app/controllers/songs/edit.js
+++ app/controllers/songs/edit.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     updateValue(value) {
-      console.log(value)
       this.set('model.song.newAlbum', value)
     }
   }
