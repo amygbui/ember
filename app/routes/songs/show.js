1bb4531d12f1d9fa0f404400e1c186b0897e0248
Amy Bui <amygbui@gmail.com>
2017-06-17 22:55:32 -0400

    Add Delete to Song

--- app/routes/songs/show.js
+++ app/routes/songs/show.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('song', params.song_id);
+  },
+  actions: {
+    deleteSong(model) {
+      this.store.findRecord('album', model.get('album.id'))
+        .then(album => {
+          album.get('songs').removeObject(model);
+          album.save();
+          model.destroyRecord();
+          this.transitionTo('albums.show', album.get('id'))
+        })
+    }
   }
 });
