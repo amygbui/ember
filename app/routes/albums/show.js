8ec5bc95db7be587b8b4677d30c0254b7c313b73
Amy Bui <amygbui@gmail.com>
2017-06-17 20:01:08 -0400

    Add delete to Album

--- app/routes/albums/show.js
+++ app/routes/albums/show.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('album', params.album_id)
+  },
+  actions: {
+    deleteAlbum(model) {
+      this.store.findRecord('artist', model.get('artist.id'))
+        .then(artist => {
+          artist.get('albums').removeObject(model);
+          artist.save();
+          model.destroyRecord();
+          this.transitionTo('artists.show', artist.get('id'))
+        })
+    }
   }
 });
