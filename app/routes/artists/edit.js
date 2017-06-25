bcdc5ac01fce59bdec24f4fd732b7e59c95427b5
Amy Bui <amygbui@gmail.com>
2017-06-25 14:56:29 -0700

    Fix Artist edit persistance

--- app/routes/artists/edit.js
+++ app/routes/artists/edit.js
@@ -2,26 +2,47 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return {
-      labels: this.store.findAll('label'),
-      artist: this.store.findRecord('artist', params.artist_id)
-    };
+    return Ember.RSVP.hash({
+      artist: this.store.findRecord('artist', params.artist_id),
+      labels: this.store.findAll('label')
+    })
+    // return {
+    //   labels: this.store.findAll('label'),
+    //   artist: this.store.findRecord('artist', params.artist_id)
+    // };
   },
   actions: {
-    updateArtist(model) {
-      this.store.findRecord('artist', model.get('id'))
-        .then(artist => {
-          artist.save();
-          if (artist.get('newLabel')) {
-            this.store.findRecord('label', artist.get('newLabel'))
-            .then(label => {
-              // label.get('artists').pushObject(artist);
-              artist.set('label', label);
-              label.save();
+    updateArtist(artist) {
+      if (artist.get('newLabel')) {
+        artist.get('label').then(oldLabel => {
+          this.store.findRecord('label', artist.get('newLabel'))
+            .then(newLabel => {
+              artist.set('label', newLabel);
+
+              if (oldLabel) {
+                oldLabel.save();
+              }
+
+              newLabel.save();
+              artist.save().then(() => this.transitionTo('artists.show', artist.get('id')))
             })
-          }
         })
-        .then(() => this.transitionTo('artists.show', model.get('id')));
+      } else {
+        artist.save();
+      }
+      // this.store.findRecord('artist', model.get('id'))
+      //   .then(artist => {
+      //     artist.save();
+      //     if (artist.get('newLabel')) {
+      //       this.store.findRecord('label', artist.get('newLabel'))
+      //       .then(label => {
+      //         // label.get('artists').pushObject(artist);
+      //         artist.set('label', label);
+      //         label.save();
+      //       })
+      //     }
+      //   })
+      //   .then(() => this.transitionTo('artists.show', model.get('id')));
     }
   }
 });
