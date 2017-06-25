a36319448e9ffd91390476d88d1d48a60fd3b28a
Amy Bui <amygbui@gmail.com>
2017-06-25 14:48:22 -0700

    Fix Song edit persistance

--- app/routes/songs/edit.js
+++ app/routes/songs/edit.js
@@ -2,25 +2,43 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return {
+    return Ember.RSVP.hash({
       song: this.store.findRecord('song', params.song_id),
       albums: this.store.findAll('album')
-    }
+    })
   },
   actions: {
-    updateSong(model) {
-      this.store.findRecord('song', model.get('id'))
-        .then(song => {
-          if (song.get('newAlbum')) {
-            this.store.findRecord('album', song.get('newAlbum'))
-              .then(album => {
-                song.set('album', album);
-                album.save();
-              })
-          }
-          song.save();
+    updateSong(song) {
+      if (song.get('newAlbum')) {
+        song.get('album').then(oldAlbum => {
+          this.store.findRecord('album', song.get('newAlbum'))
+            .then(newAlbum => {
+              song.set('album', newAlbum);
+              
+              if (oldAlbum) {
+                oldAlbum.save();
+              }
+
+              newAlbum.save();
+              song.save().then(() => this.transitionTo('songs.show', song.get('id')))
+            })
         })
-        .then(() => this.transitionTo('songs.show', model.get('id')));
+      } else {
+        song.save();
+      }
+
+      // this.store.findRecord('song', model.get('id'))
+      //   .then(song => {
+      //     if (song.get('newAlbum')) {
+      //       this.store.findRecord('album', song.get('newAlbum'))
+      //         .then(album => {
+      //           song.set('album', album);
+      //           album.save();
+      //         })
+      //     }
+      //     song.save();
+      //   })
+      //   .then(() => this.transitionTo('songs.show', model.get('id')));
     }
   }
 });
